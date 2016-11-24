9869e357c096fe2e26b4c62d9e9773820eb85911
Victor Istomin <victor.send.mail.here@gmail.com>
2016-11-24 18:38:55 +0200

    added batch files for regressin testing

---
+++ local-runner/local-runner-regression-alt.bat
@@ -0,0 +1,37 @@
+start java -Xms512m -Xmx1G -jar "local-runner.jar" local-runner-regression-alt.properties local-runner-regression-alt.default.properties
+@echo "wait for java start..."
+@timeout 10
+
+start cpp-cgdk.exe 127.0.0.1 31021 0000000000000000
+@timeout 5
+
+start cpp-cgdk.exe 127.0.0.1 31022 0000000000000000
+@timeout 5
+
+start cpp-cgdk.exe 127.0.0.1 31023 0000000000000000
+@timeout 5
+
+start cpp-cgdk.exe 127.0.0.1 31024 0000000000000000
+@timeout 5
+
+start cpp-cgdk.exe 127.0.0.1 31025 0000000000000000
+@timeout 5
+
+start cpp-cgdk-old.exe 127.0.0.1 31026 0000000000000000
+@timeout 5
+
+start cpp-cgdk-old.exe 127.0.0.1 31027 0000000000000000
+@timeout 5
+
+start cpp-cgdk-old.exe 127.0.0.1 31028 0000000000000000
+@timeout 5
+
+start cpp-cgdk-old.exe 127.0.0.1 31029 0000000000000000
+@timeout 5
+
+start cpp-cgdk-old.exe 127.0.0.1 31030 0000000000000000
+@timeout 5
+
+@echo "Done"
+pause
+
---
+++ local-runner/local-runner-regression.bat
@@ -0,0 +1,37 @@
+start java -Xms512m -Xmx1G -jar "local-runner.jar" local-runner-regression.properties local-runner-regression.default.properties
+@echo "wait for java start..."
+@timeout 10
+
+start cpp-cgdk.exe 127.0.0.1 31001 0000000000000000
+@timeout 5
+
+start cpp-cgdk.exe 127.0.0.1 31002 0000000000000000
+@timeout 5
+
+start cpp-cgdk.exe 127.0.0.1 31003 0000000000000000
+@timeout 5
+
+start cpp-cgdk.exe 127.0.0.1 31004 0000000000000000
+@timeout 5
+
+start cpp-cgdk.exe 127.0.0.1 31005 0000000000000000
+@timeout 5
+
+start cpp-cgdk-old.exe 127.0.0.1 31006 0000000000000000
+@timeout 5
+
+start cpp-cgdk-old.exe 127.0.0.1 31007 0000000000000000
+@timeout 5
+
+start cpp-cgdk-old.exe 127.0.0.1 31008 0000000000000000
+@timeout 5
+
+start cpp-cgdk-old.exe 127.0.0.1 31009 0000000000000000
+@timeout 5
+
+start cpp-cgdk-old.exe 127.0.0.1 31010 0000000000000000
+@timeout 5
+
+@echo "Done"
+pause
+
