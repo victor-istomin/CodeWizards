67f9adb3a0cd7322c02d28582c6441f54ce022dd
Victor Istomin <victor.send.mail.here@gmail.com>
2016-11-25 23:08:26 +0200

    silent mode for bat-testing

--- local-runner/local-runner-regression.bat
+++ local-runner/local-runner-regression.bat
@@ -2,34 +2,34 @@ start java -Xms512m -Xmx1G -jar "local-runner.jar" local-runner-regression.prope
 @echo "wait for java start..."
 @timeout 10
 
-start cpp-cgdk.exe 127.0.0.1 31001 0000000000000000
+start /min cpp-cgdk.exe 127.0.0.1 31001 0000000000000000
 @timeout 5
 
-start cpp-cgdk.exe 127.0.0.1 31002 0000000000000000
+start /min cpp-cgdk.exe 127.0.0.1 31002 0000000000000000
 @timeout 5
 
-start cpp-cgdk.exe 127.0.0.1 31003 0000000000000000
+start /min cpp-cgdk.exe 127.0.0.1 31003 0000000000000000
 @timeout 5
 
-start cpp-cgdk.exe 127.0.0.1 31004 0000000000000000
+start /min cpp-cgdk.exe 127.0.0.1 31004 0000000000000000
 @timeout 5
 
-start cpp-cgdk.exe 127.0.0.1 31005 0000000000000000
+start /min cpp-cgdk.exe 127.0.0.1 31005 0000000000000000
 @timeout 5
 
-start cpp-cgdk-old.exe 127.0.0.1 31006 0000000000000000
+start /min cpp-cgdk-old.exe 127.0.0.1 31006 0000000000000000
 @timeout 5
 
-start cpp-cgdk-old.exe 127.0.0.1 31007 0000000000000000
+start /min cpp-cgdk-old.exe 127.0.0.1 31007 0000000000000000
 @timeout 5
 
-start cpp-cgdk-old.exe 127.0.0.1 31008 0000000000000000
+start /min cpp-cgdk-old.exe 127.0.0.1 31008 0000000000000000
 @timeout 5
 
-start cpp-cgdk-old.exe 127.0.0.1 31009 0000000000000000
+start /min cpp-cgdk-old.exe 127.0.0.1 31009 0000000000000000
 @timeout 5
 
-start cpp-cgdk-old.exe 127.0.0.1 31010 0000000000000000
+start /min cpp-cgdk-old.exe 127.0.0.1 31010 0000000000000000
 @timeout 5
 
 @echo "Done"
